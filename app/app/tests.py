a047a92af36e8c284fe7f30d40ac0dd9772d787e
ChrMunk <16270902+ChrMunk@users.noreply.github.com>
2022-06-23 13:06:35 +0300

    fix lint errors and added core app

--- app/app/tests.py
+++ app/app/tests.py
@@ -8,14 +8,15 @@ from app import calc
 
 class CalcTests(SimpleTestCase):
     """ Test the calc module"""
+
     def test_add_numbers(self):
         """Test adding numbers together."""
-        res = calc.add(5,6)
+        res = calc.add(5, 6)
 
         self.assertEqual(res, 11)
 
     def test_substract_numbers(self):
         """test subtracting numbers."""
-        res = calc.subtract(6,5)
+        res = calc.subtract(6, 5)
 
-        self.assertEqual(res,1)
+        self.assertEqual(res, 1)
